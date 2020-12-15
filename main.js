634196734941747ecb62a898f909c5dbfb668ccd
varekaiwork <serhiisaviukwork@gmail.com>
2020-12-15 10:38:16 +0200

    hotfix

--- main.js
+++ main.js
@@ -3,10 +3,6 @@
 const link ='https://script.googleusercontent.com/macros/echo?user_content_key=rErrEIkpBrTS4k-OlPBl_DXcHN1GlV-GnIqOsWe2ph3GXsWhUYTeZDz4lqBdBYl7WsPjdtqi8Jat4KDhcB7MdTz7wIPxM62em5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHjnEmpr0NqN9j6CApA-JXQuZrhdnGnFgWbEgpErwrP7eA-aIGFrl8RZkbDMR_ovL8cV1-fvH3SfvJsw0wqpxiI&lib=MNHZN0sYz85P1xxDkZz2XELVkUTpHj_7Y'
 
 
-class Private {
-
-}
-
 
 fetch(link).then((response) => {
   return response.json();
