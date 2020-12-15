425cb4fa5ebbd8449d4350e8a8381c453e325cfc
varekaiwork <serhiisaviukwork@gmail.com>
2020-12-15 10:34:23 +0200

    change code

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 // const users = document.querySelector('#users')
 
-const link ='https://spreadsheets.google.com/feeds/cells/1hjyvgiGhJ7FPfcgy61CSfblgtmHs5DViKD7ARq3G8Gk/1/public/full?alt=json'
+const link ='https://script.googleusercontent.com/macros/echo?user_content_key=rErrEIkpBrTS4k-OlPBl_DXcHN1GlV-GnIqOsWe2ph3GXsWhUYTeZDz4lqBdBYl7WsPjdtqi8Jat4KDhcB7MdTz7wIPxM62em5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHjnEmpr0NqN9j6CApA-JXQuZrhdnGnFgWbEgpErwrP7eA-aIGFrl8RZkbDMR_ovL8cV1-fvH3SfvJsw0wqpxiI&lib=MNHZN0sYz85P1xxDkZz2XELVkUTpHj_7Y'
+
+
+class Private {
+
+}
+
 
 fetch(link).then((response) => {
   return response.json();
 })
-.then((users) => {
+.then((data) => {
 
-  const result = users.feed.entry
-  console.log(result)
-  displayUsersAsATable(result)
+  displayUsersAsATable(data)
 }).catch((e)=>{
   console.error('something wrong')
   console.log(e)
@@ -17,17 +21,24 @@ fetch(link).then((response) => {
 })
 
 
+
 function displayUsersAsATable(users) {
+
   let usersDiv = document.querySelector("#users");
 
+  // usersDiv.innerHTML = `<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>`;
+  const spinner = document.querySelector('.lds-spinner')
   let table = document.createElement("table");
 
-  users.forEach(function (currentUser) {
-    console.log(currentUser)
+  users.user.forEach( (user) =>{
       let row = table.insertRow();
       let nameCell = row.insertCell();
-      nameCell.innerHTML = currentUser.content.$t;
+      nameCell.innerHTML = user.name;
+      let winningCell = row.insertCell();
+      winningCell.innerHTML = user.winning;
   });
 
+  
   usersDiv.appendChild(table);
+    spinner.style.display='none'
 }
